8fac14d9937515f8ce2a057010198fd60fc7e033
Haiiliin <u201615255@hust.edu.cn>
2022-03-02 11:40:33 +0800

    update doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,10 +52,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-import sphinx_nameko_theme
-html_theme = 'nameko'
-html_theme_path = [sphinx_nameko_theme.get_html_theme_path()]
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
